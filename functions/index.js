9711c4f3c94aac94ba514c9842ef9d47faea1211
Tomáš Trejdl <tom.trejdl@seznam.cz>
2020-04-03 17:18:35 +0200

    Init firebase cloud functions project

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+// import * as admin from 'firebase-admin'
+
+// admin.initializeApp()
+// export const db = admin.firestore()
+
+// export * from './src/access-codes'
+
+// const functions = require('firebase-functions')
+
+// const accessCodes = require('./src/access-codes')
+
+// module.exports = {
+//   accessCodes: functions.https.onRequest(accessCodes)
+// }
